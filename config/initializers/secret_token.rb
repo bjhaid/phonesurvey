d603475d9de4b629af35bc6b3fe81720e66dab5f
Julius Francisco <baldrailers@gmail.com>
2012-04-22 14:24:39 +0800

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Phonesurvey::Application.config.secret_token = 'ec8fadcad98b97df2ab85f37ef8e3826fa6a98218c423db399ea40f78e37f98d2a42f771fe2d19234ff20ab55f4e385d4781241b6ae06bffa6c8dac35c547f39'
