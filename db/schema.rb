9e7474d3a051cefba829558629c4cd412246e9ff
Julius Francisco <baldrailers@gmail.com>
2012-04-22 17:58:10 +0800

    Adding resque tasks

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120422085321) do
+ActiveRecord::Schema.define(:version => 20120422094258) do
 
   create_table "feedbacks", :force => true do |t|
     t.integer  "survey_id"
@@ -24,9 +24,10 @@ ActiveRecord::Schema.define(:version => 20120422085321) do
   create_table "products", :force => true do |t|
     t.string   "name"
     t.string   "description"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.datetime "created_at",   :null => false
+    t.datetime "updated_at",   :null => false
     t.integer  "user_id"
+    t.string   "tropo_number"
   end
 
   create_table "roles", :force => true do |t|
