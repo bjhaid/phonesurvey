5e8f321c332b7d3192806a0734a97197f76701ad
Julius Francisco <baldrailers@gmail.com>
2012-04-27 02:10:02 +0800

    Adding refactored codes

--- lib/tropo_app.rb
+++ lib/tropo_app.rb
@@ -2,16 +2,16 @@ require "tropo-webapi-ruby"
 
 class TropoApp < Sinatra::Base
 
-  enable :sessions
+  before do
+    @v = Tropo::Generator.parse request.env["rack.input"].read
+    @t = Tropo::Generator.new do
+      on :event => 'error', :next => '/tropo/error.json'
+      on :event => 'hangup', :next => '/tropo/hangup.json'
+    end
+
+  end
 
   helpers do
-
-    def tropo_object
-      t = Tropo::Generator.new do
-        on :event => 'error', :next => '/tropo/error.json'
-        on :event => 'hangup', :next => '/tropo/hangup.json'
-      end
-    end
 
     def construct_welcome_message(product_name, msg)
       options = {
@@ -19,7 +19,7 @@ class TropoApp < Sinatra::Base
         :timeout => 60,
         :say => { :value => "Welcome to #{product_name} survey. #{product_name} is #{msg}. Do you want to take a quick survey? Yes or no." },
         :attempts => 3,
-        :choices => { :value => "yes,no" }
+          :choices => { :value => "yes,no" }
       }
     end
 
@@ -37,7 +37,7 @@ class TropoApp < Sinatra::Base
         :timeout => 30,
         :say => { :value => "Question number #{question_number}. #{msg}. #{question_appender}" },
         :attempts => 3,
-        :choices => { :value => question_options }
+          :choices => { :value => question_options }
       }
     end
 
@@ -48,71 +48,56 @@ class TropoApp < Sinatra::Base
   end
 
   post '/index.json' do
-    v = Tropo::Generator.parse request.env["rack.input"].read
-    pp v
-
-    t = tropo_object
 
     ##
     # This will detect if the next action will be outbound
     #
-    if v[:session][:parameters]
-      product = Product.find(v[:session][:parameters][:product_id])
-      t.call :to => v[:session][:parameters][:number_to_dial], :from => v[:session][:parameters][:caller_id]
-      t.on :event => 'continue', :next => "/tropo/survey.json&product_id=#{product.id}"
-      t.ask construct_welcome_message(product.name, product.description)
+    if @v[:session][:parameters]
+      product = Product.find(@v[:session][:parameters][:product_id])
+      @t.call :to => @v[:session][:parameters][:number_to_dial], :from => @v[:session][:parameters][:caller_id]
+      @t.on :event => 'continue', :next => "/tropo/survey.json&product_id=#{product.id}"
+      @t.ask construct_welcome_message(product.name, product.description)
     else
-      trunk_line = v[:session][:to][:id]
+      trunk_line = @v[:session][:to][:id]
       product = Product.find_by_tropo_number("+1#{trunk_line}")
-      t.on :event => 'continue', :next => "/tropo/survey.json&product_id=#{product.id}"
-      t.ask construct_welcome_message(product.name, product.description)
+      @t.on :event => 'continue', :next => "/tropo/survey.json&product_id=#{product.id}"
+      @t.ask construct_welcome_message(product.name, product.description)
     end
 
-    pp t.response
-    t.response
+    @t.response
   end
 
   post '/survey.json' do
-    v = Tropo::Generator.parse request.env["rack.input"].read
-    pp v
-
     product = Product.find(params[:product_id])
-    take_survey = v[:result][:actions][:take_survey][:value]
+    take_survey = @v[:result][:actions][:take_survey][:value]
 
-    t = tropo_object
     question_counter = 1
 
     case take_survey
     when 'yes'
-      t.on :event => 'continue', :next => "/tropo/results.json?product_id=#{product.id}"
+      @t.on :event => 'continue', :next => "/tropo/results.json?product_id=#{product.id}"
       products.surveys.each do |survey|
-        t.ask construct_survey("question_#{survey.id}", question_counter, survey.question, survey.yes_no)
+        @t.ask construct_survey("question_#{survey.id}", question_counter, survey.question, survey.yes_no)
         question_counter += 1
       end
     when 'no'
-      t.say :value => "Goodbye."
+      @t.say :value => "Goodbye."
     end
 
-    pp t.response
-    t.response
+    @t.response
   end
 
   post '/results.json' do
-    v = Tropo::Generator.parse request.env["rack.input"].read
-    pp v
-
     product = Product.find(params[:product_id])
 
     products.surveys.each do |survey|
-      survey_answer = v[:result][:actions]["question_#{survey.id}".to_sym][:value]
-      Feedback.create(:survey_id => survey.id, :answer => survey_answer, :callerid => v[:session][:from][:id])
+      survey_answer = @v[:result][:actions]["question_#{survey.id}".to_sym][:value]
+      Feedback.create(:survey_id => survey.id, :answer => survey_answer, :callerid => @v[:session][:from][:id])
     end
 
-    t = tropo_object
-    t.say :value => "Thank you for you time. Goodbye."
+    @t.say :value => "Thank you for you time. Goodbye."
 
-    pp t.response
-    t.response
+    @t.response
   end
 
 end
